99aba1191880d5d6bf98752f401bf1226bd4d48f
Luciano Boston <196549370+lucianobostonn@users.noreply.github.com>
2025-05-27 18:14:40 +0100

    update

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-const jsonServer = require("json-server")
+const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router("db.json")
-const middlewares = jsonServer.defaults()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults({
+    static: false
+})
 
 const PORT = process.env.PORT || 5000
 
